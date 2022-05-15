f5bb43338169f1336492183dace1bc834f4b3515
Syed Musab Mohiuddin <98490330+itsmekirx909@users.noreply.github.com>
2022-05-16 02:13:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// chap 35 to 38
+
+// Q1
+// function date(){
+//     alert(Date())
+// }
+// date()
+
+// Q2
+// function greet(first , last){
+//     alert('Welcome: ' + first + ' ' + last)
+// }
+// greet(prompt('Enter first name') , prompt('Enter last name'))
+
+// Q3
+// function sum(num1 , num2){
+
+//     var sumNum = num1 + num2
+//     alert(num1 + ' + ' + num2 + ' = ' + sumNum)
+// }
+// sum(+prompt('Enter first number') , +prompt('Enter second number'))
+
+// Q4
+// function calc(num1, op, num2) {
+//     if(op == '*'){
+//         var mul = num1 * num2
+//         alert(num1 + op + num2 + '=' + mul)
+//     }
+//     else if(op == '+'){
+//         var sum = num1 + num2
+//         alert(num1 + op + num2 + '=' + sum)
+//     }
+//     else if(op == '-'){
+//         var sub = num1 - num2
+//         alert(num1 + op + num2 + '=' + sub)
+//     }
+//     else if(op == '/'){
+//         var div = num1 - num2
+//         alert(num1 + op + num2 + '=' + div)
+//     }
+//     else if(op == '%'){
+//         var sub = num1 % num2
+//         alert(num1 + op + num2 + '=' + mod)
+//     }
+    
+// }
+// calc(+prompt('Enter first number') , prompt('Enter function') , +prompt('Enter second number'))
+
+// Q5
+// function square(num1){
+//     alert(num1 * num1)
+// }
+// square(+prompt('Enter number'))
+
+// Q6
+// var mul = 1
+// function fac(num){
+//     for(var i = 1 ; i<=num ; i++){
+//         mul =  mul * i
+//     }
+//     alert(mul)
+// }
+// fac(+prompt('Enter number'))
+
+// Q7
+// function nums(start , end){
+//     for(var i = start ; i<=end ; i++){
+//         document.write(i + '<br/>')
+//     }
+// }
+// nums(+prompt('Enter start number') , +prompt('Enter end number'))
+
+
+
+// Q8
+// function hypt(base , perp){
+//     var hyp = (base * base) + (perp * perp)
+//     alert(hyp)
+
+//     function hyptSq(){
+//         var hypSq = Math.sqrt(hyp)
+//         return hypSq
+//     }
+//     var ansSq = prompt('Answer in square root? y/n')
+//     if(ansSq == 'y'){
+//         alert(hyptSq())
+//     }
+// }
+// hypt(+prompt('Enter base') , +prompt('Enter perpendicular'))
+
+// Q9
+// function area(width , height ){
+//     var a = width * height
+//     alert(a)
+// }
+// area(2 , 2)
+
+// area(width = 4 , width = 2)
+
+// Q10
+
